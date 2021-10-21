ab24a8691fd08fbbd5a8e52f206f7644f9b42ba9
Opilane TTHK <opilane@tthk.ee>
2021-10-21 09:55:15 +0300

    FunctionsWithReturns

---
+++ EpicFight/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EpicFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero, villain;
+            string heroWeapon, villainWeapon;
+
+            hero = RandomHero();
+            heroWeapon = RandomWeapon();
+            villain = RandomVillain();
+            villainWeapon = RandomWeapon();
+            Console.WriteLine($"{hero} will fight {villain}");
+            Console.WriteLine($"{hero} will fight with {heroWeapon}.");
+            Console.WriteLine($"{villain} will fight with {villainWeapon}.");
+        }
+
+        private static int RandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static string RandomHero()
+        {
+            string[] heroes = { "Batman", "SpongeBob", "Spiderman", "Caroly" };
+            return heroes[RandomIndex(heroes)];            
+        }
+
+        private static string RandomVillain()
+        {
+            string[] villain = { "Suss", "Vadim", "TalongiPoiss", "Elise", "Sirley" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, villain.Length);
+
+            string randomVillain = villain[randomIndex];
+            return randomVillain;
+        }
+
+        private static string RandomWeapon()
+        {
+            string[] weapon = { "Gun","Knife","Boxing Glove" };
+            return weapon[RandomIndex(weapon)];
+        }
+    }
+}
---
+++ FunctionsWithReturns/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FunctionsWithReturns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] foods = { "ice cream", "chiken wings", "french fries", "burger", "salad" };
+            string[] drinks = { "coca-cola", "fanta", "sprite", "water" };
+            string[] movies = { "Truth or Dare", "Hunted Hill House", "You", "365 days" };
+            // DisplayRandomFromArray(foods);
+            // DisplayRandomFromArray(drinks);
+            string randomSnack = PickRandomFromArray(foods);
+            string randomDrink = PickRandomFromArray(drinks);
+            string randomMovies = PickRandomFromArray(movies);
+
+            Console.WriteLine($"Computer pick {randomSnack}, {randomDrink} and {randomMovies} for night");
+        }
+
+        private static string PickRandomFromArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            string randomElement = someArray[randomIndex];
+
+            return randomElement;
+
+        }
+
+        private static void DisplayRandomFromArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            Console.WriteLine($"computer has chosen {someArray[randomIndex]}");
+        }
+
+
+    }
+}
